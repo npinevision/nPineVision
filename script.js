87cf3081e9243134606aecd466350d4077e73bb6
npinevision <archie@npine.vision>
2024-07-26 10:50:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
-
 function navigateTo(page) {
     document.body.classList.add('fade-out');
-    setTimeout(() => {
-        window.location.href = page;
-    }, 500);
+    setTimeout(() => window.location.href = page, 500);
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    if (document.body.classList.contains('fade-out')) {
-        document.body.classList.remove('fade-out');
-    }
-
-    // Scroll fade-in effect
-    const portfolioItems = document.querySelectorAll('.portfolio-item');
+    document.body.classList.remove('fade-out');
 
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
@@ -23,7 +15,5 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }, { threshold: 0.1 });
 
-    portfolioItems.forEach(item => {
-        observer.observe(item);
-    });
+    document.querySelectorAll('.portfolio-item').forEach(item => observer.observe(item));
 });
